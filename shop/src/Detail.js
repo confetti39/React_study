d6e0e83d956b4ca22d6d86b00de0e1ea946c8458
“깃 <hy04730@naver.com>
2021-11-05 16:47:49 +0900

    SASS

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import './Detail.scss';
 
 let Box = styled.div`
     padding: 20px;
@@ -21,9 +22,13 @@ function Detail(props) {
         <div className="container">
 
             <Box>
-                <Title color={'red'}>Detail</Title>
-                <Title color='blue'>Detail</Title>
+                <Title className="red">Detail</Title>
+                {/* <Title color='blue'>Detail</Title> */}
             </Box>
+
+            <div className="myAlertYellow">
+                <p>재고가 얼마 남지 않았습니다!</p>
+            </div>
 
             <div className="row">
                 <div className="col-md-6">
