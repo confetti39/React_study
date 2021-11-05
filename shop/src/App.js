a63e378ba6ba85d193d4f25043469d8d47c9e4fe
“깃 <hy04730@naver.com>
2021-11-05 15:01:14 +0900

    React Router(2): Link, Switch, useHistory

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import Data from './data.js';
+import Detail from './Detail.js';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -17,8 +18,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+              <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -32,40 +33,48 @@ function App() {
       </Navbar>
 
 
+      <Switch>
 
-      {/* exact라는 속성 추가하면 경로가 정확히 일치할 때만 보여줌 */}
-      <Route exact path="/">
-        <div className="background">
-          <h1>20% season off</h1>
-          <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </div>
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((a, i) => {
-                return <Card shoes={shoes} i={i}></Card> //shoes = {a}도 사용 가능
-              })
-            }
+        {/* exact라는 속성 추가하면 경로가 정확히 일치할 때만 보여줌 */}
+        <Route exact path="/">
+          <div className="background">
+            <h1>20% season off</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
           </div>
-        </div>
-      </Route>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a, i) => {
+                  return <Card shoes={shoes} i={i}></Card> //shoes = {a}도 사용 가능
+                })
+              }
+            </div>
+          </div>
+        </Route>
 
 
-      <Route path="/detail">
-        <div>상세페이지</div>
-      </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
 
+        <Route path="/:id">
+          <div>
+            아무거나 적었을 때 보여주는 것
+          </div>
+        </Route>
 
-      {/* <Route path="/어쩌구" component={Card}></Route> */}
+        {/* <Route path="/어쩌구" component={Card}></Route> */}
 
-
+      </Switch>
 
     </div>
   );
 }
+
+
 
 function Card(props) {
   return (
