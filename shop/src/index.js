d1dc6c40e4a5203917cdf2766789f239d8fb78da
“깃 <hy04730@naver.com>
2022-02-19 18:51:02 +0900

    redux: reducer/dispatch

--- shop/src/index.js
+++ shop/src/index.js
@@ -9,7 +9,31 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-let store = createStore(() => { return [{ id: 0, name: '멋진신발', quan: 2 }] }); //state임
+let defaultState = [
+  { id: 0, name: '멋진신발', quan: 2 },
+  { id: 1, name: '예쁜신발', quan: 3 }
+]
+
+function reducer(state = defaultState, action) {
+  if (action.type === '수량증가') {
+    let copy = [...state];
+    copy[0].quan++;
+    return copy;
+  }
+  else if (action.type === '수량감소') {
+    let copy = [...state];
+    if (copy[0].quan === 0) {
+      return copy;
+    }
+    copy[0].quan--;
+    return copy;
+  }
+  else {
+    return state;
+  }
+}
+
+let store = createStore(reducer); //state임
 
 ReactDOM.render(
   <React.StrictMode>
